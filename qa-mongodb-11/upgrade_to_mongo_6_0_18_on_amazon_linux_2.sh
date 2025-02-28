2123401555583908e562c78b12b3df72f19b1d20
rod-farva-sql <33871765+rod-farva-sql@users.noreply.github.com>
2025-02-28 15:01:50 -0600

    Update upgrade_to_mongo_6_0_18_on_amazon_linux_2.sh

--- qa-mongodb-11/upgrade_to_mongo_6_0_18_on_amazon_linux_2.sh
+++ qa-mongodb-11/upgrade_to_mongo_6_0_18_on_amazon_linux_2.sh
@@ -107,8 +107,8 @@ if [[ $EUID -ne 0 ]]; then
   exit 1
 fi
 
-# Ask user if they want to upgrade the local mongodb instance from 3.6.5 to 6.0.18
-echo -e "\033[1;33mDo you want to upgrade the local mongodb instance from 3.6.5 to 6.0.18? (y/n)\033[0m"
+# Ask user if they want to upgrade the local mongodb instance from 3.6.5 to 4.0.28
+echo -e "\033[1;33mDo you want to upgrade the local mongodb instance from 3.6.5 to 4.0.28? (y/n)\033[0m"
 read -p "Enter your choice: " choice
 
 if [[ "$choice" != "y" && "$choice" != "Y" ]]; then
@@ -146,9 +146,218 @@ wait_for_mongod
 check_mongodb_version "4.0.28"
 
 #Validate the FCV is now at 4.0
-run_command "mongo --quiet --eval "db.adminCommand({ setFeatureCompatibilityVersion: '4.0' })"" "Setting FCV to 4.0"
+run_command "mongo --quiet --eval \"db.adminCommand({ setFeatureCompatibilityVersion: '4.0' })\"" "Setting FCV to 4.0"
 verify_fcv "4.0"
 
+echo -e "\033[1;32mMongoDB successfully upgraded to 4.0.28!\033[0m"
+
+
+
+
+
+
+# Ask user if they want to upgrade the local mongodb instance from 4.0.28 to 4.2.25
+echo -e "\033[1;33mDo you want to upgrade the local mongodb instance from 4.0.28 to 4.2.25? (y/n)\033[0m"
+read -p "Enter your choice: " choice
+
+if [[ "$choice" != "y" && "$choice" != "Y" ]]; then
+    echo -e "\033[1;31mExiting the script.\033[0m"
+    exit 0
+fi
+
+echo -e "\033[1;32mContinuing with upgrade...\033[0m"
+
+# Validate MongoDB version
+check_mongodb_version "4.0.28"
+
+# Upgrade from 4.0.28 to 4.2.25
+
+run_command "systemctl stop mongod" "Stopping MongoDB"
+
+run_command "wget https://repo.mongodb.org/yum/amazon/2/mongodb-org/4.2/x86_64/RPMS/mongodb-org-server-4.2.25-1.amzn2.x86_64.rpm" "Downloading mongodb-org-server 4.2.25"
+
+run_command "wget https://repo.mongodb.org/yum/amazon/2/mongodb-org/4.2/x86_64/RPMS/mongodb-org-tools-4.2.25-1.amzn2.x86_64.rpm" "Downloading mongodb-org-tools 4.2.25"
+
+run_command "wget https://repo.mongodb.org/yum/amazon/2/mongodb-org/4.2/x86_64/RPMS/mongodb-org-shell-4.2.25-1.amzn2.x86_64.rpm" "Downloading mongodb-org-shell 4.2.25"
+
+run_command "rpm -Uvh mongodb-org-server-4.2.25-1.amzn2.x86_64.rpm" "Upgrading mongodb-org-server to 4.2.25"
+
+run_command "rpm -Uvh mongodb-org-tools-4.2.25-1.amzn2.x86_64.rpm" "Upgrading mongodb-org-tools to 4.2.25"
+
+run_command "rpm -Uvh mongodb-org-shell-4.2.25-1.amzn2.x86_64.rpm" "Upgrading mongodb-org-shell to 4.2.25"
+
+run_command "sudo systemctl start mongod"
+
+# Wait for mongod to be up and running
+wait_for_mongod
+
+# Validate MongoDB version
+check_mongodb_version "4.2.25"
+
+#Validate the FCV is now at 4.2
+run_command "mongo --quiet --eval \"db.adminCommand({ setFeatureCompatibilityVersion: '4.2' })\"" "Setting FCV to 4.2"
+verify_fcv "4.2"
+
+echo -e "\033[1;32mMongoDB successfully upgraded to 4.2!\033[0m"
+
+
+
+
+
+# Ask user if they want to upgrade the local mongodb instance from 4.2.25 to 4.4.29
+echo -e "\033[1;33mDo you want to upgrade the local mongodb instance from 4.2.25 to 4.4.29? (y/n)\033[0m"
+read -p "Enter your choice: " choice
+
+if [[ "$choice" != "y" && "$choice" != "Y" ]]; then
+    echo -e "\033[1;31mExiting the script.\033[0m"
+    exit 0
+fi
+
+echo -e "\033[1;32mContinuing with upgrade...\033[0m"
+
+# Validate MongoDB version
+check_mongodb_version "4.2.25"
+
+# Upgrade from 4.2.25 to 4.4.29
+
+run_command "systemctl stop mongod" "Stopping MongoDB"
+
+run_command "wget https://repo.mongodb.org/yum/amazon/2/mongodb-org/4.2/x86_64/RPMS/mongodb-org-server-4.4.29-1.amzn2.x86_64.rpm" "Downloading mongodb-org-server 4.4.29"
+
+run_command "wget https://repo.mongodb.org/yum/amazon/2/mongodb-org/4.2/x86_64/RPMS/mongodb-org-tools-4.4.29-1.amzn2.x86_64.rpm" "Downloading mongodb-org-tools 4.4.29"
+
+run_command "wget https://repo.mongodb.org/yum/amazon/2/mongodb-org/4.2/x86_64/RPMS/mongodb-org-shell-4.4.29-1.amzn2.x86_64.rpm" "Downloading mongodb-org-shell 4.4.29"
+
+run_command "rpm -Uvh mongodb-org-server-4.4.29-1.amzn2.x86_64.rpm" "Upgrading mongodb-org-server to 4.4.29"
+
+run_command "rpm -Uvh mongodb-org-tools-4.4.29-1.amzn2.x86_64.rpm" "Upgrading mongodb-org-tools to 4.4.29"
+
+run_command "rpm -Uvh mongodb-org-shell-4.4.29-1.amzn2.x86_64.rpm" "Upgrading mongodb-org-shell to 4.4.29"
+
+run_command "sudo systemctl start mongod"
+
+# Wait for mongod to be up and running
+wait_for_mongod
+
+# Validate MongoDB version
+check_mongodb_version "4.4.29"
+
+#Validate the FCV is now at 4.4
+run_command "mongo --quiet --eval \"db.adminCommand({ setFeatureCompatibilityVersion: '4.4' })\"" "Setting FCV to 4.4"
+verify_fcv "4.4"
+
+echo -e "\033[1;32mMongoDB successfully upgraded to 4.4!\033[0m"
+
+
+
+
+
+
+# Ask user if they want to upgrade the local mongodb instance from 4.4.29 to 5.0.31
+echo -e "\033[1;33mDo you want to upgrade the local mongodb instance from 4.4.29 to 5.0.31? (y/n)\033[0m"
+read -p "Enter your choice: " choice
+
+if [[ "$choice" != "y" && "$choice" != "Y" ]]; then
+    echo -e "\033[1;31mExiting the script.\033[0m"
+    exit 0
+fi
+
+echo -e "\033[1;32mContinuing with upgrade...\033[0m"
+
+# Validate MongoDB version
+check_mongodb_version "4.4.29"
+
+# Upgrade from 4.4.29 to 5.0.31
+
+run_command "systemctl stop mongod" "Stopping MongoDB"
+
+run_command "wget https://repo.mongodb.org/yum/amazon/2/mongodb-org/4.2/x86_64/RPMS/mongodb-org-server-5.0.31-1.amzn2.x86_64.rpm" "Downloading mongodb-org-server 5.0.31"
+
+run_command "wget https://repo.mongodb.org/yum/amazon/2/mongodb-org/4.2/x86_64/RPMS/mongodb-org-tools-5.0.31-1.amzn2.x86_64.rpm" "Downloading mongodb-org-tools 5.0.31"
+
+run_command "wget https://repo.mongodb.org/yum/amazon/2/mongodb-org/4.2/x86_64/RPMS/mongodb-org-shell-5.0.31-1.amzn2.x86_64.rpm" "Downloading mongodb-org-shell 5.0.31"
+
+run_command "rpm -Uvh mongodb-org-server-5.0.31-1.amzn2.x86_64.rpm" "Upgrading mongodb-org-server to 5.0.31"
+
+run_command "rpm -Uvh mongodb-org-tools-5.0.31-1.amzn2.x86_64.rpm" "Upgrading mongodb-org-tools to 5.0.31"
+
+run_command "rpm -Uvh mongodb-org-shell-5.0.31-1.amzn2.x86_64.rpm" "Upgrading mongodb-org-shell to 5.0.31"
+
+run_command "sudo systemctl start mongod"
+
+# Wait for mongod to be up and running
+wait_for_mongod
+
+# Validate MongoDB version
+check_mongodb_version "5.0.31"
+
+#Validate the FCV is now at 5.0
+run_command "mongo --quiet --eval \"db.adminCommand({ setFeatureCompatibilityVersion: '5.0' })\"" "Setting FCV to 5.0"
+verify_fcv "5.0"
+
+echo -e "\033[1;32mMongoDB successfully upgraded to 5.0!\033[0m"
+
+
+
+
+
+
+
+
+# Ask user if they want to upgrade the local mongodb instance from 5.0.31 to 6.0.19
+echo -e "\033[1;33mDo you want to upgrade the local mongodb instance from 5.0.31 to 6.0.19? (y/n)\033[0m"
+read -p "Enter your choice: " choice
+
+if [[ "$choice" != "y" && "$choice" != "Y" ]]; then
+    echo -e "\033[1;31mExiting the script.\033[0m"
+    exit 0
+fi
+
+echo -e "\033[1;32mContinuing with upgrade...\033[0m"
+
+# Validate MongoDB version
+check_mongodb_version "5.0.31"
+
+# Upgrade from 5.0.31 to 6.0.19
+
+run_command "systemctl stop mongod" "Stopping MongoDB"
+
+run_command "wget https://repo.mongodb.org/yum/amazon/2/mongodb-org/4.2/x86_64/RPMS/mongodb-org-server-6.0.19-1.amzn2.x86_64.rpm" "Downloading mongodb-org-server 6.0.19"
+
+run_command "wget https://repo.mongodb.org/yum/amazon/2/mongodb-org/4.2/x86_64/RPMS/mongodb-org-tools-6.0.19-1.amzn2.x86_64.rpm" "Downloading mongodb-org-tools 6.0.19"
+
+run_command "wget https://repo.mongodb.org/yum/amazon/2/mongodb-org/4.2/x86_64/RPMS/mongodb-org-shell-6.0.19-1.amzn2.x86_64.rpm" "Downloading mongodb-org-shell 6.0.19"
+
+run_command "rpm -Uvh mongodb-org-server-6.0.19-1.amzn2.x86_64.rpm" "Upgrading mongodb-org-server to 6.0.19"
+
+run_command "rpm -Uvh mongodb-org-tools-6.0.19-1.amzn2.x86_64.rpm" "Upgrading mongodb-org-tools to 6.0.19"
+
+run_command "rpm -Uvh mongodb-org-shell-6.0.19-1.amzn2.x86_64.rpm" "Upgrading mongodb-org-shell to 6.0.19"
+
+run_command "sudo systemctl start mongod"
+
+# Wait for mongod to be up and running
+wait_for_mongod
+
+# Validate MongoDB version
+check_mongodb_version "6.0.19"
+
+#Validate the FCV is now at 6.0
+run_command "mongo --quiet --eval \"db.adminCommand({ setFeatureCompatibilityVersion: '6.0' })\"" "Setting FCV to 6.0"
+verify_fcv "6.0"
+
 echo -e "\033[1;32mMongoDB successfully upgraded to 6.0!\033[0m"
+
+
+
+
+
+
+
+
+
+
+
+
 
 
